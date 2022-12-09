d6eddabd584bf87ced523d2d82f36e7f7cd8924b
Moo <sukorn@gmail.com>
2022-12-09 17:05:57 +0700

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+https://blog.gopheracademy.com/advent-2017/kubernetes-ready-service/
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"example.com/api/handlers"
+	"example.com/api/version"
+)
+
+func main() {
+	log.Printf("Starting the service...\ncommit: %s, build time: %s, release: %s", version.Commit, version.BuildTime, version.Release)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
+	router := handlers.Router()
+
+	log.Print("The service is ready to listen at port " + port + " and serve.")
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
